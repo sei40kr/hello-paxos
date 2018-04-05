fef82de52059a155631d4560e5677b73a15baca9
Seong Yong-ju <sei40kr@gmail.com>
2018-04-05 19:35:22 +0900

    Update paxos.py
    Format the source code with yapf. Fix the linter errors.

--- paxos.py
+++ paxos.py
@@ -8,23 +8,26 @@ __copyright__ = 'Copyright (c) 2018 Seong Yong-ju.'
 __maintainer__ = 'Seong Yong-ju'
 __email__ = 'sei40kr@gmail.com'
 
-
 from typing import List
-from collections import Counter
+
+
+class Acceptor:
+    def prepare(self, number: int):
+        pass
 
 
 class Proposer:
-    def __init__(self, acceptors: List[Acceptor]):
+    def __init__(self, acceptors: List[Acceptor]) -> None:
         self.acceptors = acceptors
         self.number = 0
         self.value = 'proposal value'
-        self.preparing_acceptors = []
+        self.preparing_acceptors: List[Acceptor] = []
 
     # TODO This should be refactored not to have acceptors as state.
     def send_prepare_requests(self):
-      """
-Send preparation requests to all the acceptors.
-      """
+        """
+  Send preparation requests to all the acceptors.
+        """
         print('Start sending preparation requests.')
 
         # Clear the current preparing acceptors.
@@ -33,14 +36,17 @@ Send preparation requests to all the acceptors.
         max_number = -1
 
         # Send preparation requests to all the acceptors.
-        for acceptor in acceptors:
-            print('Send a preparation request to ' + str(acceptor) + ': number=' + str(self.number))
+        for acceptor in self.acceptors:
+            print('Send a preparation request to ' + str(acceptor) +
+                  ': number=' + str(self.number))
 
             try:
                 response = acceptor.prepare(self.number)
             except IOError:
-                # Ignore the acceptors that didn't accept the preparation request.
-                print('Failed to send a preparation request to ' + str(acceptor) + '.')
+                # Ignore the acceptors that didn't accept the preparation
+                # request.
+                print('Failed to send a preparation request to ' +
+                      str(acceptor) + '.')
                 continue
 
             self.preparing_acceptors.append(acceptor)
@@ -50,24 +56,21 @@ Send preparation requests to all the acceptors.
 
             if max_number < a_number:
                 max_number = a_number
-                # Remember the proposal value from the highest numbered acceptor.
+                # Remember the proposal value from the highest numbered
+                # acceptor.
                 self.value = a_value
 
-          acceptors_count = len(self.acceptors)
-          preparing_acceptors_count = len(self.preparing_acceptors)
+        acceptors_count = len(self.acceptors)
+        preparing_acceptors_count = len(self.preparing_acceptors)
 
-          print(
-              str(preparing_acceptors_count) + ' / ' + str(acceptors_count) +
-              ' are preparing.')
+        print(
+            str(preparing_acceptors_count) + ' / ' + str(acceptors_count) +
+            ' are preparing.')
 
-          if preparing_acceptors_count < acceptors_count // 2 + 1:
-              print(
-                  'The acceptors that responded to preparation requests are not majority. Aborting.'
-              )
-              return
+        if preparing_acceptors_count < acceptors_count // 2 + 1:
+            print(
+                'The acceptors that responded to preparation requests are not majority. Aborting.'
+            )
+            return
 
-          print('Finish sending preparation requests.')
-
-class Acceptor:
-  def prepare(number: int):
-    # TODO Implement this
+        print('Finish sending preparation requests.')
